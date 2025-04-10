a01636423b5ee132988e87cf78ac630fbc01ae28
Volodymyr <vova2000sk@gmail.com>
2025-04-10 11:00:12 +0300

    view df

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("start")
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+plt.rcParams['figure.figsize'] = [10, 5]
+
+df = pd.read_csv("day.csv")
+df['date'] = pd.to_datetime(df['dteday'])
+print(df)
