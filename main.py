727351406e42699cb44e3a34b8642c819c7f2f74
Volodymyr <vova2000sk@gmail.com>
2025-04-10 15:31:08 +0300

    predict data set

--- main.py
+++ main.py
@@ -34,3 +34,33 @@ plt.xlabel('Відчутна температура (atemp)')
 plt.ylabel('Кількість оренд велосипедів (cnt)')
 plt.title('Залежність між температурою та кількістю оренд велосипедів')
 plt.show()
+
+
+
+# Побудова та оційнка лінійної регресії
+training_set = df[df['date'] < '2012-06-01']
+validation_set = df[df['date'] >= '2012-06-01']
+
+training_inputs = training_set[['atemp', 'workingday', 'hum', 'weathersit']].values
+training_outputs = training_set[['cnt']].values
+
+# Створюється та навчається лінійна регресія на основі тренувального набору.
+lr = LinearRegression()
+lr.fit(training_inputs, training_outputs)
+
+validation_inputs = validation_set[['atemp', 'workingday', 'hum', 'weathersit']].values
+validation_outputs = validation_set[['cnt']].values
+
+# Виводить розсіювання справжніх та передбачених значень для валідаційного набору по датах.
+# plt.scatter(lr.predict(validation_inputs), validation_outputs)
+plt.scatter(validation_set['date'], validation_set['cnt']) # Справжні значення
+plt.scatter(validation_set['date'], lr.predict(validation_inputs)) # Передбачені значення
+plt.show()
+
+# Виводить розсіювання справжніх та передбачених значень для тренуванльного набору по датах.
+plt.scatter(training_set['date'], training_set['cnt'])
+plt.scatter(training_set['date'], lr.predict(training_inputs))
+plt.show()
+
+rmse = np.sqrt(((lr.predict(validation_inputs) - validation_outputs)**2).mean())
+print("Середньоквадратичне відхилення: ", rmse)
